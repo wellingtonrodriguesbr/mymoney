6faf664f6584eaa0313c104f453ead303bdcac73
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-05-24 19:11:38 -0300

    style: defining global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <GlobalStyle />
+    </div>
+  );
 };
